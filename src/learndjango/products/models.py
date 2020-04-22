6b9bce9b4bf4a8acbd066f79eb73edd05db13ab4
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-22 21:09:07 +0200

    changing the product models for defining the better model.

--- src/learndjango/products/models.py
+++ src/learndjango/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
     summary = models.TextField(default="this product is cool!")
