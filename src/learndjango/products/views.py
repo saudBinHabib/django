04e0ff804230ac2f020d10e32ef4a0641ac4d3e7
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-23 23:50:14 +0200

    populating the data in the template from the database.

--- src/learndjango/products/views.py
+++ src/learndjango/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Products
+
 
 # Create your views here.
+def product_detail_view(request):
+    obj = Products.objects.get(id=1)
+    context = {
+        'object': obj
+    }
+    print(obj.title)
+    return render(request, 'products/details.html', context)
