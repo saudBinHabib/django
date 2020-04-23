877f880c87c1fa3954c48e74e97f17c3bc7ff3f1
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-23 22:28:39 +0200

    adding conditional operators and forloop in the django templating engine.

--- src/learndjango/pages/views.py
+++ src/learndjango/pages/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {'my_text':  'this is a dummy text', 'my_number': 12345})
+    return render(request, "home.html", {'my_text':  'this is a dummy text', 'my_list': [12, 34, 56],
+                                         'my_number': 12345}
+                  )
 
 
 # Create your views here.
