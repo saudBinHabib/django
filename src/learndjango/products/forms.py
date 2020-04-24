e23b907ae01a08cb4bf3023f38a3651ef72178fc
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-24 20:40:47 +0200

    adding the forms and saving the data from forms.

---
+++ src/learndjango/products/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Products
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Products
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
--- src/learndjango/products/views.py
+++ src/learndjango/products/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Products
+from .forms import ProductForm
 
 
 # Create your views here.
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
+
+
 def product_detail_view(request):
     obj = Products.objects.get(id=1)
     context = {
         'object': obj
     }
-    print(obj.title)
     return render(request, 'products/product_details.html', context)
