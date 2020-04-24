6525e863204b8b50760c158e8dc3f2a9a2b4cef5
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-24 21:57:14 +0200

    playing with simple forms of html in combination with django.

--- src/learndjango/products/views.py
+++ src/learndjango/products/views.py
@@ -6,13 +6,18 @@ from .forms import ProductForm
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-    context = {
-        'form': form
-    }
+    # form = ProductForm(request.POST or None)
+    # if form.is_valid():
+    #     form.save()
+    #     form = ProductForm()
+    # context = {
+    #     'form': form
+    # }
+    title = None
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        print(title)
+    context = {'title': title}
     return render(request, 'products/product_create.html', context)
 
 
