747b24329dc861f2381bebfc37c86a9f46657da9
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-25 12:24:04 +0200

    playing with forms widget aand forms validation.

--- src/learndjango/products/forms.py
+++ src/learndjango/products/forms.py
@@ -10,3 +10,20 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Your Title'}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Your Description',
+                'class': 'new-class-name two',
+                'id': 'my-id-for-textarea',
+                'rows': 3,
+                'cols': 20,
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
--- src/learndjango/products/views.py
+++ src/learndjango/products/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render
 from .models import Products
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 
 # Create your views here.
+#
+# def product_create_view(request):
+#     # form = ProductForm(request.POST or None)
+#     # if form.is_valid():
+#     #     form.save()
+#     #     form = ProductForm()
+#     # context = {
+#     #     'form': form
+#     # }
+#     title = None
+#     if request.method == 'POST':
+#         title = request.POST.get('title')
+#         print(title)
+#     context = {'title': title}
+#     return render(request, 'products/product_create.html', context)
+
 
 def product_create_view(request):
-    # form = ProductForm(request.POST or None)
-    # if form.is_valid():
-    #     form.save()
-    #     form = ProductForm()
-    # context = {
-    #     'form': form
-    # }
-    title = None
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        print(title)
-    context = {'title': title}
+    form = RawProductForm()
+    if request.method == "POST":
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Products.objects.create(**form.cleaned_data)
+            form = RawProductForm()
+        else:
+            print(form.errors)
+    context = {
+        'form': form
+    }
     return render(request, 'products/product_create.html', context)
 
 
