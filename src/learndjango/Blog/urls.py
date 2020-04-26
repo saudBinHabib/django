0e6d9a53dc52f35228f00bea054101233bfd7488
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-26 18:33:04 +0200

    Class Based Detail View.

--- src/learndjango/Blog/urls.py
+++ src/learndjango/Blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 app_name = 'articles'
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article-list')
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- src/learndjango/Blog/views.py
+++ src/learndjango/Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -13,3 +13,13 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all()
+
+
+# Create your views here.
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    # queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Article, id=id_)
