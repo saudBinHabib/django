be77ad6c93a0e13327290ceca5a6dc6aa01b5cc7
saudBinHabib <develop.android.apps01@gmail.com>
2020-04-26 21:20:43 +0200

    adding the ClassBased A Create view for the Blog.

--- src/learndjango/Blog/urls.py
+++ src/learndjango/Blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView
 
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
     path('<int:id>', ArticleDetailView.as_view(), name='article-detail'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
 ]
--- src/learndjango/Blog/views.py
+++ src/learndjango/Blog/views.py
@@ -7,6 +7,7 @@ from django.views.generic import (
     UpdateView
 )
 from .models import Article
+from .forms import ArticleForm
 
 
 # Create your views here.
@@ -23,3 +24,15 @@ class ArticleDetailView(DetailView):
     def get_object(self):
         id_ = self.kwargs.get('id')
         return get_object_or_404(Article, id=id_)
+
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleForm
+    queryset = Article.objects.all()
+    # if you want to route the page to a specific page, not to the detail view then you can configure it here.
+    # success_url = '/blog/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
